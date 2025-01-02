aeaefed2e9bf2712318b75c54d8d94d949e70257
Bild Lopez <bildloar@gmail.com>
2025-01-01 21:39:20 -0500

    fix in volar lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,7 +6,6 @@ return {
             { "williamboman/mason.nvim", opts = {} },
             "williamboman/mason-lspconfig.nvim",
             { 'folke/neodev.nvim',       opts = {} },
-            -- "pmizio/typescript-tools.nvim",
         },
         config = function()
             -- Set up Mason before anything else
@@ -136,7 +135,16 @@ return {
                         },
                     },
                 },
-                volar = {},
+                volar = {
+                    -- add filetypes for typescript, javascript and vue
+                    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+                    init_options = {
+                        vue = {
+                            -- disable hybrid mode
+                            hybridMode = false,
+                        },
+                    },
+                },
                 tsserver = {
                     init_options = {
                         plugins = {
